75f630367c4d01f8eee68b8beba495d02ad75c41
WilliamSalcedo <wsl_1994@hotmail.com>
2025-06-12 21:17:36 -0500

    Update ForosScreen

--- src/pages/Foros/ForosScreen.jsx
+++ src/pages/Foros/ForosScreen.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header";
+import ContainerButtonRedirect from "../../components/Foro/ContainerButtonRedirect";
 
 const ForosScreen = () => {
   return (
-    <p>ForosScreen</p>
-  )
-}
+    <>
+      <Header />
+      <div style={styles.containerMain}>
+        <ContainerButtonRedirect />
+      </div>
+    </>
+  );
+};
 
-export default ForosScreen
+export default ForosScreen;
+
+const styles = {
+  containerMain: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
+};
